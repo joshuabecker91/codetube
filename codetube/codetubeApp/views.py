66ad98404bf247ff5ece2046c7361dd0ca8fb575
joshuabecker91 <joshua.becker91@gmail.com>
2022-09-17 22:44:01 -0700

    nontest login_reg and dashboard switching to real pages

--- codetube/codetubeApp/views.py
+++ codetube/codetubeApp/views.py
@@ -11,11 +11,11 @@ def index(request):
 def search(request):
     return render(request, 'search.html')
 
-def login_reg(request):
-    return render(request, 'login_reg.html')
+# def login_reg(request):
+#     return render(request, 'login_reg.html')
 
-def dashboard(request):
-    return render(request, 'dashboard.html')
+# def dashboard(request):
+#     return render(request, 'dashboard.html')
 
 def new_video(request):
     return render(request, 'new_video.html')
@@ -30,8 +30,8 @@ def play(request):
 # Login and Registration --------------------------------------------------------------------
 
 
-def testlogin_reg(request):
-    return render(request, 'testlogin_reg.html')
+def login_reg(request):
+    return render(request, 'login_reg.html')
 
 
 # Register a New User
@@ -41,17 +41,17 @@ def register(request):
         if errors:
             for error in errors:
                 messages.error(request, errors[error])
-            return redirect('/testlogin_reg')
+            return redirect('/login_reg')
         user_pw = request.POST['password']
         hash_pw = bcrypt.hashpw(user_pw.encode(), bcrypt.gensalt()).decode()
         new_user = User.objects.create(first_name=request.POST['first_name'],last_name=request.POST['last_name'],email=request.POST['email'],password=hash_pw)
         request.session['user_id'] = new_user.id
         request.session['user_name'] = f"{new_user.first_name} {new_user.last_name}"
-        return redirect('/testdashboard')
+        return redirect('/dashboard')
     return redirect('/')
 
 
-# Login
+# Login - add if already logged in then what?
 def login(request):
     if request.method=='POST':
         logged_user=User.objects.filter(email=request.POST['email'])
@@ -60,7 +60,7 @@ def login(request):
             if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
                 request.session['user_id'] = logged_user.id
                 request.session['user_name'] = f"{logged_user.first_name} {logged_user.last_name}"
-                return redirect('/testdashboard')
+                return redirect('/dashboard')
             else:
                 messages.error(request,"Password is incorrect.")
         else:
@@ -75,7 +75,7 @@ def logout(request):
 
 
 # Dashboard - Check if Logged in
-def testdashboard(request):
+def dashboard(request):
     if 'user_id' not in request.session:
         return redirect('/')
     user = User.objects.get(id=request.session['user_id'])
@@ -84,7 +84,7 @@ def testdashboard(request):
         'all_videos': Video.objects.all(),
         # 'all_likes': Liked.objects.all()
     }
-    return render(request,'testdashboard.html', context)
+    return render(request,'dashboard.html', context)
 
 
 # Videos and Likes --------------------------------------------------------------------------
