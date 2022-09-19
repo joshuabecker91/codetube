98e40030eb5284286740b11a98a6da8ae6936899
Summer King <sdawnk08@hotmail.com>
2022-09-18 19:13:31 -0700

    9/18 html and css updates, commented out admin path

--- codetube/codetube/urls.py
+++ codetube/codetube/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 from codetubeApp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('codetubeApp.urls')),
 ]
